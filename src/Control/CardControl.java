5911d4a37ea7cf6b03c9004a7433bf5e2d08a2ab
natt666 <nataaaliaia@gmail.com>
2025-05-26 19:56:47 +0200

    S'han afegit diferentes accions com robarCarta, assignar la imgBack o Front, afegit un mètode a classe Joker, i redistribucicó dels metodes i classes.

--- src/Control/CardControl.java
+++ src/Control/CardControl.java
@@ -6,13 +6,13 @@ import Model.Warrior;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.sql.*;
 import java.util.*;
 import java.util.List;
 
 public class CardControl {
-    public static void main(String[] args) {
-    }
 
     public static Deque<Card> CreateStack(){
         String db_url = "jdbc:mysql://localhost:3306/toaiwa";
@@ -91,4 +91,95 @@ public class CardControl {
             i++;
         }
     }
+
+    public static class CambiarMano extends MouseAdapter {
+        private final Card[] manoJug;
+        private final Deque<Card> pila;
+        private final JLabel[] manoJugLabel;
+
+        public CambiarMano (Card[] manoJug, Deque<Card> pila, JLabel[] manoJugLabel) {
+            this.manoJug = manoJug;
+            this.pila = pila;
+            this.manoJugLabel = manoJugLabel;
+        }
+
+        @Override
+        public void mouseClicked(MouseEvent e) {
+            System.out.println("Cambiando mano...");
+
+            for (int i = 0; i < manoJug.length; i++) {
+                if (!pila.isEmpty()) {
+                    manoJug[i] = CardControl.robarCarta(pila);
+                    CardControl.AsignarImgFront(manoJugLabel[i], manoJug[i], manoJugLabel[i].getWidth(), manoJugLabel[i].getHeight());
+                } else {
+                    System.out.println("La pila está vacía, no se puede robar más");
+                }
+            }
+        }
+    }
+
+    public static class AnadirMouseAdapterDestino extends MouseAdapter{
+
+        public void mouseClicked(MouseEvent e, Card[] cartasEnDestino,Card cartaOrigen, int indiceCartaOrigen,
+                                 JLabel destinoLabel, int index, Card[] manoJug, Deque<Card>pila, JLabel[] manoJugLabel) {
+
+            super.mouseClicked(e);
+            Card cartaDestino = cartasEnDestino[index];
+
+            for (Card c : cartasEnDestino){
+                System.out.println(c);
+            }
+
+            if (cartaOrigen == null || indiceCartaOrigen == -1) {
+                System.out.println("No hay carta seleccionada para mover.");
+                return;
+            }
+
+            if (cartaDestino == null) {
+                cartasEnDestino[index] = cartaOrigen;
+
+                AsignarImgFront(destinoLabel, cartaOrigen, destinoLabel.getWidth(), destinoLabel.getHeight());
+                manoJug[indiceCartaOrigen] = robarCarta(pila);
+                AsignarImgFront(manoJugLabel[indiceCartaOrigen], manoJug[indiceCartaOrigen], manoJugLabel[indiceCartaOrigen].getWidth(), manoJugLabel[indiceCartaOrigen].getHeight());
+                indiceCartaOrigen = -1;
+            } else {
+                if (cartaOrigen instanceof Warrior && cartaDestino instanceof Warrior) {
+                    Warrior wOrigen = (Warrior) cartaOrigen;
+                    Warrior wDestino = (Warrior) cartaDestino;
+
+                    if ((wOrigen.getColor().equals(wDestino.getColor())) && (wOrigen.getValue() != wDestino.getValue())) {
+                        cartasEnDestino[index] = cartaOrigen;
+                        AsignarImgBack(destinoLabel, cartaOrigen, destinoLabel.getWidth(), destinoLabel.getHeight());
+                        manoJug[indiceCartaOrigen] = robarCarta(pila);
+                        AsignarImgFront(manoJugLabel[indiceCartaOrigen], manoJug[indiceCartaOrigen], manoJugLabel[indiceCartaOrigen].getWidth(), manoJugLabel[indiceCartaOrigen].getHeight());
+                        indiceCartaOrigen = -1;
+                    } else {
+                        System.out.println("La carta origen no puede reemplazar a la carta destino: valor menor o igual.");
+                    }
+                } else {
+                    System.out.println("La carta origen y destino tienen el mismo color, no se puede reemplazar.");
+                }
+            }
+            cartaOrigen = null;
+            cartaDestino = null;
+        }
+    }
+
+    public static void AsignarImgBack(JLabel label, Card carta, int width, int height) {
+        ImageIcon icono = new ImageIcon(carta.getImgBack());
+        Image imgEscalada = icono.getImage().getScaledInstance(width, height, Image.SCALE_SMOOTH);
+        label.setIcon(new ImageIcon(imgEscalada));
+    }
+
+    public static void AsignarImgFront(JLabel label, Card carta, int ancho, int alto) {
+        ImageIcon icono = new ImageIcon(carta.getImgFront());
+        Image imagen = icono.getImage().getScaledInstance(ancho, alto, Image.SCALE_SMOOTH);
+        label.setIcon(new ImageIcon(imagen));
+    }
+
+    public static Card robarCarta (Deque<Card>pila){
+        Card  carta = pila.pollLast();
+        return carta;
+    }
+
 }
